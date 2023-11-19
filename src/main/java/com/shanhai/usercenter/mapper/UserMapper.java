502e05f5d1e9a8fc20167c5652566cddc227830e
haibeiqingchuan <17340031319@163.com>
2023-11-19 21:13:53 +0800

    项目搭建

---
+++ src/main/java/com/shanhai/usercenter/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.shanhai.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shanhai.usercenter.model.User;
+
+/**
+ * @description: mapper接口层
+ * @author: hetianyou
+ * @date: 2023/11/19 20:47
+ * @Version: 1.0
+ **/
+public interface UserMapper extends BaseMapper<User> {
+
+}
